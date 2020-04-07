afafca8e759b87452868b103b17ede3c13de9e4f
Dino <aldinorahman36@gmail.com>
2020-04-07 11:52:24 +0700

    dasd

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import Axios from 'axios'
 import {APIURL} from './supports/UrlApi'
-import {Table} from 'reactstrap'
+import {Table,CustomInput} from 'reactstrap'
 
 
 const initialstate={
@@ -27,20 +27,48 @@ const reducer=(state=initialstate,action)=>{
 function App() {
   const [state,dispatch]=useReducer(reducer,initialstate)
   const [data,setdata]=useState([])
+
+  const [addimagefile,setimageadd]=useState({
+    addImageFileName:'pilih foto..',
+    addImageFile:undefined,
+  })
+
   useEffect(()=>{
-    Axios.get(`${APIURL}/allusers`)
+    Axios.get(`${APIURL}/foto/foto`)
     .then((res)=>{
       console.log(res.data)
       setdata(res.data)
     })
   },[])
 
-  const addDataClick=()=>{
-    var obj={
-      username:state.username,
-      password:state.password
+
+  const onAddImageFileChange=(event)=>{
+    // console.log(document.getElementById('addImagePost').files[0])
+    console.log(event.target.files[0])
+    var file=event.target.files[0]
+    if(file){
+        setimageadd({...addimagefile,addImageFileName:file.name,addImageFile:event.target.files[0]})
+    }else{
+        setimageadd({...addimagefile,addImageFileName:'pilih foto',addImageFile:undefined})
     }
-    Axios.post(`${APIURL}/users`,obj)
+}
+
+  const addDataClick=()=>{
+    var formdata=new FormData()
+    const data={
+      caption:state.password,
+    }
+
+    var Headers={
+      headers:
+      {
+          'Content-Type':'multipart/form-data',
+        //   'Authorization':`Bearer ${token}`
+      },
+    }
+    formdata.append('image',addimagefile.addImageFile)
+    formdata.append('data',JSON.stringify(data))
+    Axios.post(`${APIURL}/foto/foto`,formdata,Headers)
     .then((res)=>{
       setdata(res.data)
     }).catch((err)=>{
@@ -49,7 +77,7 @@ function App() {
     })
   }
   const deletedataClick=(id)=>{
-    Axios.delete(`${APIURL}/users/${id}`)
+    Axios.delete(`${APIURL}/users/users/${id}`)
     .then((res)=>{
       setdata(res.data)
     })
@@ -59,8 +87,8 @@ function App() {
       return(
         <tr key={index}>
           <td>{index+1}</td>
-          <td>{val.username}</td>
-          <td>{val.password}</td>
+          <td><img src={APIURL+val.imagefoto} height='200px'/></td>
+          <td>{val.caption}</td>
           <td><button className='btn btn-primary'>edit</button> <button className='btn btn-danger' onClick={()=>deletedataClick(val.id)}>Delete</button></td>
         </tr>
       )
@@ -82,8 +110,8 @@ function App() {
         </tbody>
         <tfoot>
           <tr>
-            <td><input type='text' value={state.username} onChange={(e)=>dispatch({type:'username',payload:e.target.value})} placeholder='username'/></td>
-            <td><input type='text' value={state.password} onChange={(e)=>dispatch({type:'password',payload:e.target.value})} placeholder='password'/></td>
+            <td><CustomInput id='foto' type='file' label={addimagefile.addImageFileName} onChange={onAddImageFileChange}/></td>
+            <td><input type='text' value={state.password} onChange={(e)=>dispatch({type:'password',payload:e.target.value})} placeholder='caption'/></td>
             <td><button className='btn btn-success' onClick={addDataClick}> add</button></td>
           </tr>
         </tfoot>
